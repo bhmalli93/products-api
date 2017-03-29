15b2267a79152b7888ef7f802cca41f34e5aee77
bhmalli93 <npuRAM93>
2017-03-28 19:40:15 -0500

    JUnit Test Cases

--- src/test/java/com/product/api/test/ProductServiceTest.java
+++ src/test/java/com/product/api/test/ProductServiceTest.java
@@ -1,24 +1,15 @@
 package com.product.api.test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import com.product.api.bootstrap.Bootstrap;
-import com.product.api.dao.ProductRepository;
 import com.product.api.exception.Fault;
-import com.product.api.model.CurrentPrice;
-import com.product.api.model.ProductResponse;
 import com.product.api.model.Products;
 import com.product.api.service.ProductService;
 
@@ -28,53 +19,27 @@ public class ProductServiceTest {
 	
 	@Autowired
     private ProductService productService;
-	@Mock
-	ProductRepository productRepository;
-	@MockBean
-	RestTemplate restTemplate;
-	@MockBean
-	Fault fault;
 	
-	Long id = (long) 13860428;
-	Products product;	 	
- 	CurrentPrice  currentPrice;
- 	@MockBean
- 	ProductResponse response;
+	@Autowired
+	private Fault  fault;
 	
-	@Before
- 	public void setup(){
- 		product= new Products();
- 		currentPrice = new CurrentPrice();
- 		response = new ProductResponse();
- 		/*currentPrice.setCurrency_code("USD");
- 		currentPrice.setValue("13.99");
- 		product.setId("13860428");
- 		product.setCurrent_price(currentPrice);*/
- 		
- 	}
-	
-	 @Test
-	    public void productShouldReturnFromProductService() throws Exception {
-		 
-		// String URI = "http://redsky.target.com/v2/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
-		 
-		 product =productService.getProduct(id);
-		 
-		 assertEquals("13860428", product.getId());
-	    }
-	 
-	 @Test
-	    public void productShouldReturnFromProductServices() throws Exception {
-		 
-		 String URI = "http://redsky.target.com/v2/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
+	Products product;
+	     
+	@Test
+	public void testGetProduct_NotFound(){	
+	product =	productService.getProduct((long) 16483589);	
+	Assert.assertEquals(HttpStatus.NOT_FOUND, fault.getCode());	
 		
-	    }
-
+	}
 	
+	@Test
+	public void testGetProduct_Found(){	
+	product =	productService.getProduct((long) 13860428);	
+	Assert.assertNotNull(product);
+	Assert.assertEquals("USD",product.getCurrent_price().getCurrency_code());	
+	Assert.assertEquals("The Big Lebowski (Blu-ray)", product.getName());	
 	
-	
-	    
-        
-    
+		
+	}
 
 }
